e31ba1d522444441e4d52a43d7d009fc166093a9
TchappiR <tchappiromaric@gmail.com>
2026-04-15 10:43:51 +0200

    remove printing

--- training_model.py
+++ training_model.py
@@ -6,7 +6,6 @@ import joblib  # Pour sauvegarder le modèle
 
 def train_model():
     # 1. Chargement des données
-    print("Chargement des données Iris...")
     iris = datasets.load_iris()
     
     # X = Longueur pétale, y = Largeur pétale
@@ -19,7 +18,6 @@ def train_model():
     )
 
     # 3. Initialisation et Entraînement
-    print("Entraînement du modèle de régression linéaire...")
     regr = linear_model.LinearRegression()
     regr.fit(X_train, y_train)
 
@@ -28,14 +26,9 @@ def train_model():
     mse = mean_squared_error(y_test, y_pred)
     r2 = r2_score(y_test, y_pred)
 
-    print(f"--- Résultats ---")
-    print(f"MSE: {mse:.4f}")
-    print(f"R2 Score: {r2:.4f}")
-
     # 5. Sauvegarde du modèle (Format .joblib)
     model_filename = "regression_iris.joblib"
     joblib.dump(regr, model_filename)
-    print(f"Modèle sauvegardé sous : {model_filename}")
 
 if __name__ == "__main__":
     train_model()
